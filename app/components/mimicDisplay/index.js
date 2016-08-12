c0b6827639f82c69625215cf301dc07d2254e2ae
wecodeingit <contactuiconsultants@gmail.com>
2016-08-12 21:08:50 +0530

    added mimic display base

--- app/components/mimicDisplay/index.js
+++ app/components/mimicDisplay/index.js
@@ -5,32 +5,30 @@
 
 
  module.exports = {
-     init: function() {
+     init: function($el) {
          var paper = new joint.dia.Paper({
-             el: $('body'),
-             width: 600,
-             height: 200,
+             el: $el,
+             width: 400,
+             height: 40,
              model: graph,
              gridSize: 1
          });
 
          var rect = new joint.shapes.basic.Rect({
-             position: { x: 100, y: 30 },
+             position: { x: 0, y: 10 },
              size: { width: 100, height: 30 },
-             attrs: { rect: { fill: 'blue' } }
+             attrs: { rect: { fill: 'black' } }
          });
-
 
          var rect2 = rect.clone();
          rect2.translate(300);
-
 
          var link = new joint.dia.Link({
              source: { id: rect.id },
              target: { id: rect2.id }
          });
 
-         graph.addCells([rect]);
+         graph.addCells([rect, rect2, link]);
 
      }
  };
--- app/electrical/index.js
+++ app/electrical/index.js
@@ -2,10 +2,16 @@
 var $ = require('jquery');
 var tmpl = require("../utilities/components-layout/tmpl.html");
 var helpers = require("../utilities/handlebars/helpers.js");
+var mimicDisplay = require("../components/mimicDisplay");
 module.exports = {
     init: function(el) {
+        this.$el = $(el);
         var data = { title: "Electrical" };
         var template = helpers.compile(tmpl);
-        $(el).html(template(data));
+        this.$el.html(template(data));
+        this.loadMimicDisplay();
+    },
+    loadMimicDisplay: function() {
+        mimicDisplay.init(this.$el.find(".mimicDisplay"));
     }
 };
